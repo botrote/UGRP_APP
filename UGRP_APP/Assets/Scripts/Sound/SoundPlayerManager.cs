b55b3c8df65327a141eb2b2985a16057ce54e14d
botrote <symbol0328@gmail.com>
2020-07-18 19:50:04 +0900

    simple sound player added (feature 3)

---
+++ UGRP_APP/Assets/Scripts/Sound/SoundPlayerManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundPlayerManager : MonoBehaviour
+{
+    AudioSource audioPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioPlayer = gameObject.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void EnrollSound(string filename)
+    {
+        AudioClip audioFile = Resources.Load("sound/" + filename) as AudioClip;
+        audioPlayer.clip = audioFile;
+    }
+}
---
+++ UGRP_APP/Assets/Scripts/UI/Input_SoundSelect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputField : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Text inputText;
+    SoundPlayerManager sManager;
+    void Start()
+    {
+        inputText = transform.Find("Text").gameObject.GetComponent<Text>();
+        sManager = GameObject.Find("AudioPlayManager").GetComponent<SoundPlayerManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnEndEdit()
+    {
+        sManager.EnrollSound(inputText.text);
+    }
+}
