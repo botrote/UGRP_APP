53db8cbbcc18989e9d0e7acfd66eec5d16dcbdf6
botrote <symbol0328@gmail.com>
2020-07-18 23:53:54 +0900

    simple sound recoder added

--- UGRP_APP/Assets/Scripts/Sound/SoundPlayerManager.cs
+++ UGRP_APP/Assets/Scripts/Sound/SoundPlayerManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class SoundPlayerManager : MonoBehaviour
 {
@@ -14,12 +15,36 @@ public class SoundPlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void EnrollSound(string filename)
     {
+        /*
         AudioClip audioFile = Resources.Load("sound/" + filename) as AudioClip;
         audioPlayer.clip = audioFile;
+        */
+        StartCoroutine(EnrollSound_C(filename));
+        
+    }
+
+    private IEnumerator EnrollSound_C(string filename)
+    {
+        if (!filename.ToLower().EndsWith(".wav")) 
+        {
+			filename += ".wav";
+		}
+        string path = Path.Combine(Application.persistentDataPath + "/data/", filename);
+
+        WWW tempWWW = new WWW(@"file://" + path);
+        
+        Debug.Log(path);
+
+        while(!tempWWW.isDone)
+            yield return null;
+        
+        audioPlayer.clip = tempWWW.GetAudioClip(false, false);
+
+        yield return null;
     }
 }
---
+++ UGRP_APP/Assets/Scripts/Sound/SoundRecorder.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+public class SoundRecorder : MonoBehaviour
+{
+    AudioSource audio;
+    string clipName;
+    Text SaveText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = gameObject.GetComponent<AudioSource>();
+        SaveText = GameObject.Find("Canvas").transform.Find("SaveText").GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void RecordStart()
+    {
+        audio.clip = Microphone.Start(Microphone.devices[0], false, 60, 44100);
+    }
+
+    public void RecordEnd()
+    {
+        Microphone.End(Microphone.devices[0]);
+        clipName = System.DateTime.Now.ToString().Replace("-", "").Replace(":", "").Replace(" ", "").Replace("P", "").Replace("M", "_");
+        audio.clip.name = clipName;
+    }
+
+    public void RecordPlay()
+    {
+        audio.Play();
+    }
+
+    public void RecordSave()
+    {
+        
+        SavWav.Save(clipName, audio.clip);
+        SaveText.enabled = true;
+        SaveText.text = "ClipName " + clipName + " saved"; 
+        
+    }
+}
