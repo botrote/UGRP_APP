7af03e652a0075c87f5707127b1846692900fab8
botrote <symbol0328@gmail.com>
2020-07-19 00:05:38 +0900

    파일 위치 정리

--- UGRP_APP/Assets/Scripts/UI/Input_SoundSelect.cs
+++ UGRP_APP/Assets/Scripts/UI/Input_SoundSelect.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class InputField : MonoBehaviour
+public class Input_SoundSelect : MonoBehaviour
 {
     // Start is called before the first frame update
     Text inputText;
