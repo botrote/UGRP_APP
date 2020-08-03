1ca402f66e1dd0a14fe4c19f9e797715572541dc
botrote <symbol0328@gmail.com>
2020-08-03 14:49:59 +0900

    Audioclip serialization

---
+++ UGRP_APP/Assets/Scripts/Sound/AudioSerializer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.IO;
+using System;
+
+public class AudioSerializer : MonoBehaviour
+{
+    public bool isLoading { get; private set; }
+    public byte[] loadedAudio { get; private set; }
+    public AudioSource audioSource;
+
+    void Start()
+    {
+
+    }
+
+    public IEnumerator LoadAudioClipToByte(string fileName)
+    {
+        isLoading = true;
+        if (!fileName.ToLower().EndsWith(".wav")) 
+        {
+			fileName += ".wav";
+		}
+        string path = Path.Combine(Application.persistentDataPath + "/data/", fileName);
+
+        WWW tempWWW = new WWW(@"file://" + path);
+        
+        Debug.Log(path);
+
+        while(!tempWWW.isDone)
+                yield return null;
+        
+        AudioClip clip = tempWWW.GetAudioClip(false, false);
+        audioSource.clip = clip;
+        float[] soundData = new float[clip.samples * clip.channels];
+        clip.GetData(soundData, 0);
+
+        loadedAudio = new byte[soundData.Length * 4];
+        Buffer.BlockCopy(soundData, 0, loadedAudio, 0, loadedAudio.Length);
+        isLoading = false;
+    }
+
+    public AudioClip StoreByteClip(string fileName, byte[] data, int samples, int channels)
+    {
+        float[] soundData = new float[data.Length / 4];
+        Buffer.BlockCopy(data, 0, soundData, 0, data.Length);
+        AudioClip clip = AudioClip.Create(fileName, samples, channels, 44100, false);
+        clip.SetData(soundData, 0);
+        return clip;
+    }
+}
--- UGRP_APP/Assets/Scripts/Sound/SoundRecorder.cs
+++ UGRP_APP/Assets/Scripts/Sound/SoundRecorder.cs
@@ -9,6 +9,9 @@ public class SoundRecorder : MonoBehaviour
     string clipName;
     Text StatusText;
     // Start is called before the first frame update
+
+    float recordStartTime;
+    float recordEndTime;
     void Start()
     {
         audio = gameObject.GetComponent<AudioSource>();
@@ -23,6 +26,7 @@ public class SoundRecorder : MonoBehaviour
 
     public void RecordStart()
     {
+        recordStartTime = Time.time;
         audio.clip = Microphone.Start(Microphone.devices[0], false, 60, 44100);
         StatusText.enabled = true;
         StatusText.text = "Recording..";
@@ -30,8 +34,28 @@ public class SoundRecorder : MonoBehaviour
 
     public void RecordEnd()
     {
-        Microphone.End(Microphone.devices[0]);
-        clipName = System.DateTime.Now.ToString().Replace("-", "").Replace(":", "").Replace(" ", "").Replace("P", "").Replace("M", "_");
+        recordEndTime = Time.time;
+        AudioClip recordedClip = audio.clip;
+        int position = Microphone.GetPosition(Microphone.devices[0]);
+        float[] soundData = new float[recordedClip.samples * recordedClip.channels];
+        recordedClip.GetData (soundData, 0);
+        float[] newData = new float[position * recordedClip.channels];
+ 
+
+        for (int i = 0; i < newData.Length; i++) 
+        {
+            newData[i] = soundData[i];
+        }
+
+        Debug.Log("clip length : " + newData.Length);
+
+        AudioClip newClip = AudioClip.Create(recordedClip.name, position, recordedClip.channels, recordedClip.frequency, false);
+        newClip.SetData(newData, 0);
+
+        AudioClip.Destroy(audio.clip);
+        audio.clip = newClip;
+
+        clipName = System.DateTime.Now.ToString().Replace("-", "").Replace(":", "").Replace(" ", "").Replace("P", "").Replace("A", "").Replace("M", "_");
         audio.clip.name = clipName;
         StatusText.enabled = true;
         StatusText.text = "Recording finished";
