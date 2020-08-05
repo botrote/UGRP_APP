a707a3cf244c1c60885395acbab03061e47a1735
suah1126 <suah5375@gmail.com>
2020-08-05 15:03:16 +0900

    오디오 파일 포맷팅

--- UGRP_APP/Assets/Scripts/Sound/AudioSerializer.cs
+++ UGRP_APP/Assets/Scripts/Sound/AudioSerializer.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using UnityEngine;
 using System.IO;
 using System;
+using System.Text;
 
 public class AudioSerializer : MonoBehaviour
 {
@@ -15,6 +16,13 @@ public class AudioSerializer : MonoBehaviour
     void Start()
     {
 
+    }
+
+    private IEnumerator Test()
+    {
+        yield return StartCoroutine(LoadAudioClipToByte("20200804_63934"));
+        yield return new WaitForSeconds(5);
+        StoreByteClip(loadedAudio);
     }
 
     public IEnumerator LoadAudioClipToByte(string fileName)
@@ -32,23 +40,63 @@ public class AudioSerializer : MonoBehaviour
 
         while(!tempWWW.isDone)
                 yield return null;
-        
+
         AudioClip clip = tempWWW.GetAudioClip(false, false);
         audioSource.clip = clip;
         float[] soundData = new float[clip.samples * clip.channels];
         clip.GetData(soundData, 0);
+        
+        /*
+         * | sample=4 byte | channel=4byte | filenameLength=4 byte | fileName=n byte | data=m byte |
+         * 
+         *
+         */
 
-        loadedAudio = new byte[soundData.Length * 4];
-        Buffer.BlockCopy(soundData, 0, loadedAudio, 0, loadedAudio.Length);
+        byte[] b_samples =  BitConverter.GetBytes(clip.samples);
+        byte[] b_channels = BitConverter.GetBytes(clip.channels);
+        byte[] b_fileNameLength = BitConverter.GetBytes(fileName.Length);
+        byte[] b_fileName= Encoding.UTF8.GetBytes(fileName);
+
+        loadedAudio = new byte[soundData.Length * 4 + 12 + b_fileName.Length];
+
+        Buffer.BlockCopy(b_samples, 0, loadedAudio, 0, 4);
+        Buffer.BlockCopy(b_channels, 0, loadedAudio, 4, 4);
+        Buffer.BlockCopy(b_fileNameLength, 0, loadedAudio, 8, 4);
+        Buffer.BlockCopy(b_fileName, 0, loadedAudio, 12, b_fileName.Length);
+        Buffer.BlockCopy(soundData, 0, loadedAudio, 12+b_fileName.Length, loadedAudio.Length-12-b_fileName.Length);
+
         isLoading = false;
     }
 
-    public AudioClip StoreByteClip(string fileName, byte[] data, int samples, int channels)
+    public AudioClip StoreByteClip(byte[] data)
     {
-        float[] soundData = new float[data.Length / 4];
-        Buffer.BlockCopy(data, 0, soundData, 0, data.Length);
+      //  Debug.Log(data.Length);
+        byte[] b_samples = new byte[4];
+        byte[] b_channels = new byte[4];
+        byte[] b_fileNameLength = new byte[4];
+    //    byte[] fileName = new byte[];
+
+        Buffer.BlockCopy(data, 0, b_samples, 0, 4);
+        Buffer.BlockCopy(data, 4, b_channels, 0, 4);
+        Buffer.BlockCopy(data, 8, b_fileNameLength, 0, 4);
+
+        int samples =BitConverter.ToInt32(b_samples, 0);
+        int channels =BitConverter.ToInt32(b_channels, 0);
+        int fileNameLength = BitConverter.ToInt32(b_fileNameLength, 0);
+
+        byte[] b_fileName = new byte[fileNameLength];
+        float[] soundData = new float[(data.Length-12-fileNameLength) / 4];
+
+        Buffer.BlockCopy(data, 12, b_fileName, 0, fileNameLength);
+        string fileName = Encoding.UTF8.GetString(b_fileName);
+        Buffer.BlockCopy(data, 12+fileNameLength, soundData, 0, data.Length-12-fileNameLength);
+
+      
+
         AudioClip clip = AudioClip.Create(fileName, samples, channels, 44100, false);
         clip.SetData(soundData, 0);
+        audioSource.clip = clip;
+        SavWav.Save(fileName, clip);
         return clip;
     }
 }
